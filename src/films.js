280e04d261e2a5798ab689b5519e0d499178a9b2
monicab1989 <monika_291@hotmail.com>
2022-03-02 15:14:22 +0100

    Ejercicios

--- src/films.js
+++ src/films.js
@@ -1,28 +1,75 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result = array.map(name=>name.director);
+  //console.log("EXERCISE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  /*let pelicula=[];
+  for(let i=0;i<array.length;i++){
+    if(array[i].director===director){
+      pelicula.push(array[i]);  
+    }
+  }
+  console.log("EX2 ", pelicula);
+  return pelicula;*/
+  const films = array.filter(item=>item.director===director);
+  //console.log("Ex2 ", films);
+  return films;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
+  /*let contador=0;
+  let sum = 0;
+  let nota=0;
+  for(let i=0;i<array.length;i++){
+    if(array[i].director===director){
+      sum=sum + array[i].score;
+      contador++
+    }
+    nota=sum/contador;
+  }
+  console.log("EX3 " + nota.toFixed(2));
+  return nota.toFixed(2);*/
   
+  let nota = array.reduce((acc,item)=>{
+    //let contador=0;
+    //console.log(acc);
+    let sum = [];
+    if(item.director===director && item.score!=[]){
+      sum.push(item.score);
+      acc++;
+     // console.log("EX·",acc);
+    }
+   console.log(acc + item.score);
+    return sum/acc;
+  },0);
+  //console.log("EX3 " + contador);
+  return nota;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  const titles = array.map(name=>name.title);
+  let titlesB=[]
+  for(let i=0;i<titles.length;i++){
+    if(i<20){
+      titlesB.push(titles[i]);
+    }
+  }
+  titlesB.sort();
+  //console.log(array);
+  return titlesB;
+  //console.log(titles);
+  //return titles;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+//console.log(array);
 }
 
 // Exercise 6: Calculate the average of the movies in a category
